b116e4f19cd8ac6e5d92243ee65215542f04c01a
arthurdiasdeveloper <arthurdiascorporativo@gmail.com>
2023-01-09 14:15:01 -0300

    config babel and webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,35 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-//a sintaxe de importação ainda é antiga. o Babel não vai interpretar o webpack
-//escrevemos dessa forma para garantir que o node ou qualquer lugar que esteja sendo utilizado 
-//as configurações ele seja execultado.
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack'); //to access built-in plugins
+
 module.exports = {
-    //minhas ferramentas de desenvolvimento
-    devtools: "source-map",
-    //onde meu arquivo principal estará
-    enty: "./src/inde.js",
-    //e os modules serão nossos modúlos que 
-    //seráo nossas regras
-    modules: {
-        reules: [{
-                // validação: todos os aquivos que tenham JS eu quero....
-                test: /\.js$/,
-                //eu quero primeiramente excluir a leitura disso dentro do meu node modules (no
-                //caso são os pacotes que a gente já instalou. Esses pacotes geralmente já são tratados.)
-                exclude: /node_modules/,
-                //desejamos usar o babel loader porque no nosso js o babel terà que fazer a transpilação para
-                // que todos os navegadores entendam.
-                use: {
-                    loader: "babel-loader"
-                }
-            },
+  devtools: "source-map",
+  module: {
+    rules: [{ test: /\.js$/, 
+    exclude: /node_modules/,
+    use: 'babel-loader' },
 
-            {
-                test: /\.html$/,
-                use: [{
-                    loader: "html-loader"
-                }]
-            },
+    { test: /\.html$/, 
+    exclude: /node_modules/,
+    use: 'html-loader' },
 
-            {
-                test: /\.css$/,
-                use: ["style-loader", "css-loader"]
-            },
+    { test: /\.css$/, 
+    exclude: /node_modules/,
+    use: ['style-loader', 'css-loader'] },
 
-            {
-                test: /\.(png|jpe?g|gif))$/i,
-                use: ["file-loader"]
-            },
+    { test: /\.(png|jpe?g|gif)$/i, 
+    exclude: /node_modules/,
+    use: ['file-loader'] },
 
-        ]
-    },
-    resolve: {
-        extensions: [".js", ".jsx"],
+    { resolve: /\.html$/, 
+    extensions: [".js", ".jsx"]
+    
+    }
 
-    },
-    plugin: [
-        //a variavel que criamos ali em cima. os plugins a gente importa e utiliza aqui dentro.
-        new HtmlWebpackPlugin ({
-            //o template é o resultado final. Onde nosso còdigo será exportado.
-            template: './public/index.html'
-        })
-    ]
-}
+
+],
+    
+
+   
+  },
+  plugins: [new HtmlWebpackPlugin({ template: './src/index.html' })],
+};
